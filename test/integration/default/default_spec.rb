fb92dd552688a20aabfe7b94ea9a3d08420629e4
Mimoun Chikhi <mimoun.chikhi@ext.actelion.com>
2016-03-19 19:59:51 +0100

    m

--- test/integration/default/default_spec.rb
+++ test/integration/default/default_spec.rb
@@ -1,10 +1,7 @@
-require 'serverspec'
-
-set :backend, :exec
+require 'spec_helper'
 
 describe 'tested' do
   it 'responds on port 2222' do
     expect(port 2222).to be_listening 'tcp'
   end
-
 end
